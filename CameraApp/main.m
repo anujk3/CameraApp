90088d1de10132cc82defce6cdd00bc9d1b1d454
Anuj Katiyal <anujk3@gmail.com>
2014-09-19 11:11:33 +0530

    Initial Commit

---
+++ CameraApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CameraApp
+//
+//  Created by Anuj Katiyal on 19/09/14.
+//  Copyright (c) 2014 Katiyals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
